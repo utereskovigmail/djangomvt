177e6539bcf19704a87156400a65f159a2689f6b
George <utereskovygmail.com@MacBook-Pro.local>
2026-03-19 16:54:25 +0200

    crud for products

--- atbmvt/products/urls.py
+++ atbmvt/products/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('add/', views.create_product, name='add'),
     path('upload-temp/', views.upload_temp_image, name='upload-temp'),
     path('delete-temp/', views.delete_temp_image, name='delete-temp'),
+    path('', views.list_products, name='list_products'),
+    path('edit/<slug:slug>/', views.edit_product, name='edit'),
+    path('delete/<slug:slug>/', views.delete_product, name='delete'),
+
 ]
