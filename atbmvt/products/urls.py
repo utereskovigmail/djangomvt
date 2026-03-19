ad50852defd406441bf067268b80cb2b5cb5cbce
George <utereskovygmail.com@MacBook-Pro.local>
2026-03-19 12:05:10 +0200

    added product creation

--- atbmvt/products/urls.py
+++ atbmvt/products/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name='products'
 
 urlpatterns = [
-    path('add/', views.add_product, name='add_product'),
-    path("upload_temp_image/", views.upload_temp_image, name="upload_temp_image"),
+    path('add/', views.create_product, name='add'),
+    path('upload-temp/', views.upload_temp_image, name='upload-temp'),
+    path('delete-temp/', views.delete_temp_image, name='delete-temp'),
 ]
